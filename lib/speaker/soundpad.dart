432e7b3ff076cbf940ea2648c1055e42af6c8607
Kartik-Excellence <kartik@excellencetechnologies.info>
2020-11-30 12:33:45 +0530

    @@

--- lib/speaker/soundpad.dart
+++ lib/speaker/soundpad.dart
@@ -17,12 +17,11 @@ class _SoundPadState extends State<SoundPad> {
         prefix: "assets/",
         fixedPlayer: AudioPlayer()..setReleaseMode(ReleaseMode.STOP));
   }
-  
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
+    return Column(
+      children: <Widget>[
         GridView.builder(
             padding: EdgeInsets.symmetric(horizontal: 5, vertical: 15),
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -43,17 +42,41 @@ class _SoundPadState extends State<SoundPad> {
               );
             },
           ),
-        
-        Center(
-          child: IconButton(
-              iconSize: 60,
-              color: Colors.black,
-              icon: Icon(Icons.play_arrow),
-              onPressed: () {
-               
-              }),
-        )
       ],
     );
+    // return Stack(
+    //   children: [
+        // GridView.builder(
+        //     padding: EdgeInsets.symmetric(horizontal: 5, vertical: 15),
+        //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        //       crossAxisCount: 3,
+        //       childAspectRatio: MediaQuery.of(context).size.width /
+        //           (MediaQuery.of(context).size.height / 4),
+        //     ),
+        //     itemCount: 9,
+        //     itemBuilder: (context, index) {
+        //       return Container(
+        //         padding: EdgeInsets.all(5),
+        //         child: RaisedButton(
+        //           onPressed: () {
+        //             audioCache.play('sounds/DTMF${index + 1}.mp3');
+        //           },
+        //           child: Center(child: Text('$index')),
+        //         ),
+        //       );
+        //     },
+        //   ),
+
+    //     Center(
+    //       child: IconButton(
+    //           iconSize: 60,
+    //           color: Colors.black,
+    //           icon: Icon(Icons.play_arrow),
+    //           onPressed: () {
+
+    //           }),
+    //     )
+    //   ],
+    // );
   }
 }
